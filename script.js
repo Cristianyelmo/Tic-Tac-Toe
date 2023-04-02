b88d374f00ae5950e2f454715a66702a21579b23
Cristianyelmo <cristianama2019@gmail.com>
2023-04-02 19:52:52 -0300

    Tic-tac-toe casi terminado

---
+++ script.js
@@ -0,0 +1,181 @@
+const selectBox = document.querySelector('.select-box'),
+selectXBtn = selectBox.querySelector('.playerX'),
+selectOBtn = selectBox.querySelector('.playerO'),
+playBoard=document.querySelector('.play-board' ),
+allBox = document.querySelectorAll('section span'),
+players = document.querySelector('.players');
+resultBox = document.querySelector('.result-box'),
+wonText = resultBox.querySelector('.won-text'),
+replayBtn = resultBox.querySelector('button')
+
+window.onload = () =>{
+for (let i = 0; i < allBox.length; i++) {
+    allBox[i].setAttribute('onclick','clickedBox(this)')   
+}
+    selectXBtn.onclick = ()=>{
+selectBox.classList.add('hide')
+playBoard.classList.add('show')
+}
+selectOBtn.onclick = ()=>{
+    selectBox.classList.add('hide')
+    playBoard.classList.add('show')
+    playBoard.setAttribute('class','players active player')
+}
+
+}
+
+let playerXIcon = "fas fa-times";
+let playerOIcon = "far fa-circle";
+let playerSign = 'X';
+let runBot = true
+
+// user click function
+
+function clickedBox(element){
+    console.log(element)
+    
+    if(players.classList.contains('player')){
+        
+
+       /*  element.innerHTML = `<i class="${playerOIcon}">O</i>` */
+
+       element.innerHTML = `<img class="imagen"src="/perrito.png" alt="">`
+       players.classList.add('active');
+       playerSign = 'O';
+       element.setAttribute('id',playerSign)
+
+    }else{
+        element.innerHTML = `<img class="imagen"src="/gatito.png" alt="">`;
+        players.classList.add('active');
+        element.setAttribute('id',playerSign)
+    }
+    selectWinner();
+
+    playBoard.style.pointerEvents = 'none' ;
+ element.style.pointerEvents = 'none' ;
+ let randomDelaytime=((Math.random()* 1000 ) +20).toFixed()
+ 
+ setTimeout(()=>{
+bot(runBot)
+
+ },randomDelaytime)
+
+}
+
+
+//bot click function
+
+
+function bot(runBot){
+    
+    
+    
+    if(runBot){
+
+
+
+        playerSign = 'O'
+        let array =[];
+        for (let i = 0; i < allBox.length; i++) {
+           
+            if(allBox[i].childElementCount == 0){
+        array.push(i);
+            }
+          
+        }
+        let randomBox = array[Math.floor(Math.random() * array.length)]
+        
+        
+        
+        
+        
+        if(array.length >0){
+        
+            if(players.classList.contains('player')){
+        
+                /*  element.innerHTML = `<i class="${playerOIcon}">O</i>` */
+         
+                allBox[randomBox].innerHTML = `<img class="imagen"src="/gatito.png" alt="">`
+                players.classList.remove('active');
+        
+                playerSign = 'X';
+                allBox[randomBox].setAttribute('id',playerSign)
+                
+         
+             }else{
+                allBox[randomBox].innerHTML = `<img class="imagen"src="/perrito.png" alt="">`;
+                 players.classList.remove('active');
+                 allBox[randomBox].setAttribute('id',playerSign)
+             }
+             selectWinner();
+        }
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = 'auto' ;
+        playerSign = 'X'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+function getClass(idname){
+return document.querySelector('.box' + idname).id;
+}
+
+function checkClass(val1,val2,val3,sign){
+    
+    if(getClass(val1)== sign && getClass(val2)== sign && getClass(val3)== sign){
+        return true
+    }
+
+}
+
+function selectWinner(){
+    
+    if(checkClass(1,2,3,playerSign) || checkClass(4,5,6,playerSign) || checkClass(7,8,9,playerSign)|| checkClass(1,4,7,playerSign)|| checkClass(2,5,8,playerSign)|| checkClass(3,6,9,playerSign)|| checkClass(1,5,9,playerSign)|| checkClass(3,5,7,playerSign)){
+        console.log(playerSign + ' ' + 'is the winner');
+        runBot = false;
+        bot(runBot);
+        
+        
+        
+
+        setTimeout(()=>{
+
+playBoard.classList.remove('show');
+resultBox.classList.add('show')
+
+
+
+        },700)
+
+
+        
+        
+        
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`
+    }
+
+
+
+
+
+}
+
+
+replayBtn.onclick = ()=>{
+    window.location.reload()
+}
